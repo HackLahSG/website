5009ca1a61d14c1a38133e1f67c6597fd90a7e11
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-12 10:46:34 +0800

    Header Image

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -4,8 +4,11 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div>
-      <Image src={'/images/hero.jpg'} alt="Lion" width={1300} height={700} className="grid-container"/>
+    <div className='header'>
+      <Image src='/images/logo.png' alt='Logo' width={900} height={288} className='head-logo'/>
+      <h2 className="head-text" >Join us for a day of Hacking</h2>
+      <h1 className="head-text" >  March 18th 2023 <br/>9:00am–9:00pm</h1>
+      <h3 className="head-text" >???, Singapore </h3>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Header from './components/header'
 
 export default function Home() {
   return (
-  <div className="in-progress-container">
-    <Header />
+  <div>
+    <Header/>
   </div>
   ) 
   }
