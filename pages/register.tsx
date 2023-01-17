0b4813be6bba46a6b928fa4031e9a4d6b854b389
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-18 07:52:17 +0800

    Registration page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,17 +1,8 @@
 import Image from 'next/image' 
 export default function Register() {
   return (
-    <div className='bg'>
-    <Image src="/images/logo.png" alt="Picture of the author" width={700} height={250} className="logo" />
-    <h1 className='regtext'>Let&apos;s get you registered!</h1>
-    <div className='email'>
-    <h1>Email</h1>
-    <input type='text'></input>
-    </div> 
-    <div className='fullname'>
-    <h1>Full name</h1>
-    <input type='text'></input>
-    </div>
+    <div className="bg">
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScWmtThZlEbIszOLOUUH5yModrGSozj1Yz_8wXRIhEyUQ3_JQ/viewform?embedded=true" width="640" height="1400" className="form">Loading…</iframe>
     </div>
   ) 
 }
