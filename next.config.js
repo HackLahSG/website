a348d7c228058d74646a7bd12185b421efe62c3a
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-18 08:22:52 +0800

    image debug

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: "akamai",
-    path: "/",
-   },
+    domains: [
+      'www.your-domain-name.com',
+    ]
+  },
 }
 
 module.exports = nextConfig
