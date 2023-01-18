c0612fbbd0de03221f1405938ad6f2ec9c861716
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-18 08:24:58 +0800

    image debug

--- pages/register.tsx
+++ pages/register.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 export default function Register() {
   return (
     <div className="bg">
-    <Image src="/images/logo.png" alt="Picture of the author" width={700} height={250} className="logo" />
+    <Image src={"/images/logo.png"} alt="Picture of the author" width={700} height={250} className="logo" />
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScWmtThZlEbIszOLOUUH5yModrGSozj1Yz_8wXRIhEyUQ3_JQ/viewform?embedded=true" width="640" height="1400" className="form">Loading…</iframe>
     </div>
   ) 
