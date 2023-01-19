280969d69be82859e91a744f3896b4fec77738f1
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-19 11:02:18 +0800

    mobile ver. header

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      'www.your-domain-name.com',
-    ]
-  },
+
 }
 
 module.exports = nextConfig
