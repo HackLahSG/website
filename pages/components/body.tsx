67a71ffb7046ced0e16f1b550ab44551fd71d562
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-23 00:32:17 +0800

    responsive-1

--- pages/components/body.tsx
+++ pages/components/body.tsx
@@ -12,11 +12,8 @@ const Body = () => {
         <h1 className={`titles details`}>Details</h1>
         <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5632.912827907666!2d103.82352216397369!3d1.2528980125220164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19557bd3cdd1%3A0x4738e8af5f70f2c2!2sBattlestar%20Galactica!5e0!3m2!1sen!2ssg!4v1673619662790!5m2!1sen!2ssg"
-        width={453}
-        height={375}
-        style={{ border: 0, marginLeft:950, marginTop: 200}}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"/>
+        referrerPolicy="no-referrer-when-downgrade"
+        className="map-container"/>
         <h2 className={`infotext infotext2`}>hello</h2>
     </div>
   )
