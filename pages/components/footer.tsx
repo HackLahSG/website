5374053974f4f272340f81e7704372e17451d5c0
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-23 03:52:01 +0800

    DONE!!! ONLY FINAL TOUCHES LEFT

---
+++ pages/components/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h1 className={`titles footertxt`}>Ready to hack?</h1>
+      <Link href="/register"><input type="button" value="Register Now" className="RegButton footerbutton"></input></Link>
+    <div>
+        <p className={`infotext footbox`}>Made with ❤️ by the ️HackLah! Team &emsp;&emsp;HackLah! is fiscally sponsored by The Hack Foundation, a 501(c)(3) nonprofit. Our registered EIN is 81-2908499.</p>
+    </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from './components/header'
 import Body from './components/body'
+import Footer from './components/footer'
 
 export default function Home() {
   return (
   <div className='bg'>
     <Header/>
     <Body/>
+    <Footer/>
   </div>
   ) 
   }
