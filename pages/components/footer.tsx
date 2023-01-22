e685252d6f629476fb010cb772079e86c639232e
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-23 04:06:37 +0800

    disabled EIN becoming a link

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <h1 className={`titles footertxt`}>Ready to hack?</h1>
       <Link href="/register"><input type="button" value="Register Now" className="RegButton footerbutton"></input></Link>
     <div>
-        <p className={`infotext footbox`}>Made with ❤️ by the ️HackLah! Team &emsp;&emsp;HackLah! is fiscally sponsored by The Hack Foundation, a 501(c)(3) nonprofit. Our registered EIN is 81-2908499.</p>
+        <p className={`infotext footbox disable-link`}>Made with ❤️ by the ️HackLah! Team &emsp;&emsp;HackLah! is fiscally sponsored by The Hack Foundation, a 501(c)(3) nonprofit. Our registered EIN is 81-2908499.</p>
     </div>
     </div>
   )
