598d4a0a2689565a26d438e63d9fd40739c5df9b
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-23 13:50:12 +0800

    previewtest2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta property="og:image" content="/images/preview.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <body className="bg">
         <Main />
         <NextScript />
