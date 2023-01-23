e5e03f70e18d816ecf42040ff363183e948554d1
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-23 13:53:01 +0800

    previewtest3

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
       <meta property="og:image" content="/images/preview.png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+      </Head>
       <body className="bg">
         <Main />
         <NextScript />
