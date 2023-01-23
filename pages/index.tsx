842905aa2273e020bdf84aafcc02063449750142
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-23 13:40:14 +0800

    preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Header from './components/header'
 import Body from './components/body'
 import Footer from './components/footer'
+import Head from 'next/head'
 
 export default function Home() {
   return (
   <div className='bg'>
+    <meta property="og:image" content="/images/preview.png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
     <Header/>
     <Body/>
     <Footer/>
