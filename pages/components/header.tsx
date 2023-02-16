6a169442c77ca12a32fbd897b4b995a6f3f2a16e
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-02-16 17:57:48 +0800

    Update header.tsx

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className='header'>
       <Image src='/images/logo.png' alt='Logo' width={900} height={288} className='head-logo'/>
       <h2 className={`head-text h2a`} >Join us for a day of Hacking</h2>
-      <h1 className={`head-text h1a`} >  March 18th 2023 <br/>9:00am–9:00pm</h1>
+      <h1 className={`head-text h1a`} >  June 3rd 2023 <br/>9:00am–9:00pm</h1>
       <h3 className={`head-text h3a`} >???, Singapore </h3>
       <Link href="/register"><input type="button" value="Register Now" className="RegButton"></input></Link>
     </div>
