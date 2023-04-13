3982baa7c0c76e9fa38e34b497c1e6bbe3765090
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-04-14 00:16:32 +0800

    Animations added

--- pages/components/body.tsx
+++ pages/components/body.tsx
@@ -1,36 +1,44 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { relative } from 'path'
+import Marquee from 'react-fast-marquee';
 
 const Body = () => {
   return (
-
     <div>
-        <h1 className={`titles title1`}>What is HackLah!?</h1>
-        <h2 className={`infotext infotext1`}>HackLah! is a full-day hackathon event hosted by students, for students around the tiny red dot — Singapore. Come together to build projects and share them with the world!
-        Let&apos;s make one thing clear: we don&apos;t mean hack as in gain unauthorized access and take things, we mean create and tinker something new.</h2>
-        <Image src={"/images/image1.png"} id='image1'alt="image1" width={530} height={300} className='image1 max-w-full h-auto'/>
-        <h1 className={`titles details`}>Details</h1>
-        <iframe
+      <h1 className="titles title1 bounce">What is HackLah!?</h1>
+      <h2 className="infotext infotext1">
+        HackLah! is a full-day hackathon event hosted by students, for students around the tiny red dot — Singapore. Come together to build projects and share them with the world!
+        Let&apos;s make one thing clear: we don&apos;t mean hack as in gain unauthorized access and take things, we mean create and tinker something new.
+      </h2>
+      <Image src={"/images/image1.png"} id='image1'alt="image1" width={530} height={300} className='image1 max-w-full h-auto'/>
+      <h1 className="titles details bounce">Details</h1>
+      <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5632.912827907666!2d103.82352216397369!3d1.2528980125220164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19557bd3cdd1%3A0x4738e8af5f70f2c2!2sBattlestar%20Galactica!5e0!3m2!1sen!2ssg!4v1673619662790!5m2!1sen!2ssg"
         referrerPolicy="no-referrer-when-downgrade"
-        className="map-container"/>
-        <h2 className={'infotext detailstext'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        <Image src={"/images/FAQ.png"} alt="faq" width={530} height={300} className='faq-container'/>
-        <div className='center'>
-        <h1 className={`titles sponsors`}>Sponsors:</h1>
+        className="map-container"
+      />
+      <h2 className="infotext detailstext">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </h2>
+      <Image src={"/images/FAQ.png"} alt="Frequently Asked Questions" width={530} height={300} className="faq-container"/>
+      <div className="center">
+        <h1 className="titles sponsors">Sponsors:</h1>
+      </div>
+      <Marquee speed={40} gradient={false}>
+        <div className="images">
+          <Link href="https://gen.xyz/"><Image src={"/images/xyz.png"} alt=".xyz" width={210} height={123} style={{paddingLeft: 60}} className="" /></Link>
+          <Link href="https://artofproblemsolving.com/online"><Image src={"/images/aops.png"} alt="Art of Problem Solving" width={250} height={100} style={{paddingLeft: 60}} className="" /></Link>
+          <Link href="https://www.taskade.com/"><Image src={"/images/taskade.png"} alt="Taskade" width={123} height={123} style={{paddingLeft: 60}} className="" /></Link>
+          <Link href="https://www.leading-learners.com/"><Image src={"/images/LL.png"} alt="Leading Learners" width={492} height={123} style={{paddingLeft: 60}} className="" /></Link>
+          <Link href="https://www.echo3d.com/"><Image src={"/images/echo3D.png"} alt="Echo3D" width={250} height={40} style={{paddingLeft: 60, paddingBottom: 30}} className="" /></Link>
         </div>
-        <div className='images'>
-        <Link href="https://gen.xyz/"><Image src={"/images/xyz.png"} alt=".xyz" width={210} height={123} style={{paddingLeft: 60}} className={''}/></Link>
-        <Link href="https://artofproblemsolving.com/online"><Image src={"/images/aops.png"} alt="aops" width={250} height={100} style={{paddingLeft: 60}} className={''}/></Link>
-        <Link href="https://www.taskade.com/"><Image src={"/images/taskade.png"} alt="taskade" width={123} height={123} style={{paddingLeft: 60}}className={''}/></Link>
-        <Link href="https://www.leading-learners.com/"><Image src={"/images/LL.png"} alt="LL" width={248} height={62} style={{paddingLeft: 60}}className={''}/></Link>
-        <Link href="https://www.echo3d.com/"><Image src={"/images/echo3D.png"} alt="echo3D" width={250} height={40} style={{paddingLeft: 60}}className={''}/></Link>
-        </div>
-        <p className={`infotext prospectus`}>Want to support us? Read our sponsorship <Link href="https://drive.google.com/file/d/1FnQeNZlfaIBwlwxDYzhMCnBtcm333man/view?usp=sharing" >prospectus</Link> and then reach out to us at <Link href="mailto: aarav@hacklah.com">aarav@hacklah.com</Link> or donate <Link href='https://bank.hackclub.com/donations/start/hacklahsg'>here</Link></p>
-
-        <Link href="https://hackclub.com/bank/"><Image src={"/images/hcb.png"} alt="hcb" width={530} height={300} className='hcb'/></Link>
+      </Marquee>
+      <p className="infotext prospectus">
+        Want to support us? Read our sponsorship <Link href="https://drive.google.com/file/d/1FnQeNZlfaIBwlwxDYzhMCnBtcm333man/view?usp=sharing">prospectus</Link> and then reach out to us at <Link href="mailto: aarav@hacklah.com">aarav@hacklah.com</Link>
+        or donate <Link href='https://bank.hackclub.com/donations/start/hacklahsg'>here</Link>.
+      </p>
+      <Link href="https://hackclub.com/bank/"><Image src={"/images/hcb.png"} alt="Hack Club Bank" width={530} height={300} className="hcb"/></Link>
     </div>
   )
 }
--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <div className='footer'>
-      <h1 className={`titles footertxt`}>Ready to hack?</h1>
+      <h1 className={`titles footertxt bounce`}>Ready to hack?</h1>
       <Link href="/register"><input type="button" value="Register Now" className="RegButton footerbutton"></input></Link>
     <div>
         <p className={`infotext footbox disable-link`}>Made with ❤️ by the ️HackLah! Team &emsp;&emsp;HackLah! is fiscally sponsored by The Hack Foundation, a 501(c)(3) nonprofit. Our registered EIN is 81-2908499.</p>
