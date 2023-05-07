3566e67cd9002f1cde8dd91de279ac13621df9c0
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-05-07 16:19:28 +0800

    time

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className='header'>
       <Image src='/images/logo.png' alt='Logo' width={900} height={288} className='head-logo'/>
       <h2 className={`head-text h2a`} >Join us for a day of Hacking:</h2>
-      <h1 className={`head-text h1a`} >  June 3rd 2023 <br/>9:00am–9:00pm</h1>
+      <h1 className={`head-text h1a`} >3rd of June 1200hrs - 4th of June 1000hrs<br/>SGT</h1>
       <a href="#details"><h3 className={`head-text h3a`} >Online, Singapore </h3></a>
       <Link href="/register">
 <button className='RegButton'>Register</button>
